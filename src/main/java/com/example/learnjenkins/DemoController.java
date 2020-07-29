9ccfc6181ec3738463c5d0ef069eacf8b3356535
PYQ <1138827104@qq.com>
2020-07-29 16:52:00 +0800

    init

---
+++ src/main/java/com/example/learnjenkins/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.learnjenkins;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pengYongQiang
+ * @version 1.0
+ * @date 2020/7/29 16:49
+ */
+@RestController
+public class DemoController {
+
+    @RequestMapping("demo")
+    public String demo(){
+        return "hellp";
+    }
+}
